fe6878a2f0e0ebb973a726132004790be2bb3472
Meow <mklim9866@gmail.com>
2019-11-26 18:59:11 +0800

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,32 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-let stickyHeader = new StickyHeader();
+
+
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll('.open-modal').forEach(el => {
+  el.addEventListener('click', e => {
+
+    e.preventDefault();
+
+    if (typeof modal == 'undefined') {
+      // promise function
+      import(/* webpackChunkName: 'modal' */ './modules/Modal').then(x => {
+
+        modal = new x.default();
+        modal.openTheModal();
+      }).catch(() => console.log('There was a problem.'));
+    } else {
+      modal.openTheModal();
+    }
+
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
